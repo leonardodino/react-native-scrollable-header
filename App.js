abe3d8f752a1884c10f5150065a09397c6f60cfe
Leonardo Dino <leonardo@kunstdesign.com.br>
2017-08-30 16:00:37 -0300

    fix header height

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import Expo from 'expo';
+import {
+  Constants,
+} from 'expo';
 import React, { Component } from 'react';
 import {
   Animated,
@@ -10,7 +12,9 @@ import {
 } from 'react-native';
 
 const HEADER_MAX_HEIGHT = 300;
-const HEADER_MIN_HEIGHT = Platform.OS === 'ios' ? 60 : 73;
+const HEADER_MIN_HEIGHT = Platform.OS === 'ios'
+  ? 44 + Constants.statusBarHeight
+  : 48 + Constants.statusBarHeight
 const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
 
 export default class App extends Component {
